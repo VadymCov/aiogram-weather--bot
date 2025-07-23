2686ab9532d679f6b2be8fdab3d45972b5380d2c
VadymCov <vadymkravtcsov@gmail.com>
2025-07-23 22:25:54 +0300

    feat: add async weather function for city lookup

--- bot.py
+++ bot.py
@@ -1,18 +1,35 @@
 import asyncio 
+import aiohttp ##
 from aiogram import Bot # this is a class for communicating with the Telegram API
-from aiogram import Dispatcher # this component that links incoming updates and handlers.
+from aiogram import Dispatcher  # this component that links incoming updates and handlers.
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from config import YOUR_TOKEN # Important: config.py is added to .gitignore to prevent token leakage.  
-
+from config import WEATHER_API_KEY # Important: config.py is added to .gitignore to prevent API-KEY leakage.
 
 bot = Bot(token = YOUR_TOKEN,)
 dp = Dispatcher()
 
+async def get_weather(city: str)->str:
+    url = (
+        "http://api.openweathermap.org/data/2.5/weather"
+    f"?q={city}&appid={WEATHER_API_KEY}&units=metric&lang=eng"
+    )
+    async with aiohttp.ClientSession() as session: # creates a session for HTTP requests
+        async with session.get(url) as response: # sends a GET request
+            data = await response.json() # waits for a response and turns it into a Python dictionary
+    if response.status != 200:
+        return '📛error'
+    descr = data["weather"][0]["descriptions"].capitalize() # first element of the weather array
+    temp = data["main"]["temp"]
+    feels_like = data["main"]["feels_like"]
+    return f"weather in {city} - {descr}: '{temp}°c feels like {feels_like}'" # display the weather beautifully on the screen
+
 
 @dp.message(CommandStart)
 async def start_handler(message:Message):
-    await message.answer("Hello i`m bot weather")
+    await message.answer(f"Hello, {message.from_user.first_name}.\ni`m bot weather")
+
 
 
 async def main():
