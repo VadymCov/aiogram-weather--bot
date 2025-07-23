085f713031673e62c009266ab0bdf1ffbf5725f9
VadymCov <vadymkravtcsov@gmail.com>
2025-07-23 19:47:00 +0300

    Add basic weather bot with start command handler

--- bot.py
+++ bot.py
@@ -1,9 +1,22 @@
 import asyncio 
 from aiogram import Bot # this is a class for communicating with the Telegram API
 from aiogram import Dispatcher # this component that links incoming updates and handlers.
-from config import TOKEN # Important: config.py is added to .gitignore to prevent token leakage.  
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from config import YOUR_TOKEN # Important: config.py is added to .gitignore to prevent token leakage.  
 
-bot = Bot(token = TOKEN)
-dp = Dispatcher
+
+bot = Bot(token = YOUR_TOKEN,)
+dp = Dispatcher()
 
 
+@dp.message(CommandStart)
+async def start_handler(message:Message):
+    await message.answer("Hello i`m bot weather")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
